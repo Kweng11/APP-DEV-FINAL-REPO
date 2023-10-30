bc705d795fc8eadcf9c8372a34118ee3b3a7326c
Kweng11 <144525375+Kweng11@users.noreply.github.com>
2023-10-30 16:02:17 +0800

    End-user done
    fix the login

---
+++ src/Agrisync/HeoAbout.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="hero_area">
+      <div class="hero_bg_box">
+           <img src="@/assets/agrisync/images/hero-bg.jpg" alt="">
+                       
+      </div>
+      <header class="header_section" style="position: fixed; top: 0; left: 0; z-index: 9999; width: 100%; height: 100px; background-color: #00a087;">
+        <div class="logo" style="position: fixed; top: 0; left: 0;height: 0; width: 10;">
+               <img src="@/assets/agrisync/images/B.png" height="100" width="100">
+                <a href="/Login"> <img src="@/assets/agrisync/images/L.png" height="100" width="100"></a>
+             
+            </div>
+
+        <div class="container">
+            <nav class="navbar navbar-expand-lg custom_nav-container ">
+            <a class="navbar-brand" href="/">
+                <span>
+                Agrisync
+                </span>
+            </a>
+            
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class=""> </span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <span></span>
+                <li class="nav-item  active ">
+                    <a class="nav-link" href="/About "> About</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/Contact">Contact</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link"  href="/Login">Login</a>
+            </li>
+                <form class="form-inline">
+                    <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                </form>
+                </ul>
+            </div>
+            </nav>
+        </div>
+        </header>
+
+
+
+
+
+      
+      </div>
+</template>
---
+++ src/Login/Log.vue
@@ -0,0 +1,69 @@
+<template>
+  <br>
+  <br>
+  <br>
+      
+
+  <section class="about_section layout_margin-top">
+  <div class="container  ">
+    <div class="row">
+      <div class="col-md-6 ">
+        <div class="img-box">
+          <img src="@/assets/agrisync/images/di.png" alt="">
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="detail-box">
+          <div class="heading_container">
+           
+            <h1> Log In</h1>
+            <form>
+<!-- Email input -->
+<div class="form-outline mb-4">
+<input type="email" id="form2Example1" class="form-control" />
+<label class="form-label" for="form2Example1">Email address</label>
+</div>
+
+<!-- Password input -->
+<div class="form-outline mb-4">
+<input type="password" id="form2Example2" class="form-control" />
+<label class="form-label" for="form2Example2">Password</label>
+</div>
+
+<!-- 2 column grid layout for inline styling -->
+<div class="row mb-4">
+<div class="col d-flex justify-content-center">
+<!-- Checkbox -->
+<div class="form-check">
+<input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+<label class="form-check-label" for="form2Example31"> Remember me </label>
+</div>
+</div>
+
+<div class="col">
+<!-- Simple link -->
+<a href="/">Forgot password?</a>
+</div>
+</div>
+
+<!-- Submit button -->
+<button type="button"  href="/User" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+<!-- Register buttons -->
+<div class="text-center">
+<p>Not a member? <a href="/">Register</a></p>
+</div>
+</form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<br>
+<br>
+<br>
+
+
+</template>
